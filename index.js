77faac752d58ab68bcddb3709a3e383dbdc32979
Titus Wormer <tituswormer@gmail.com>
2020-12-21 17:33:57 +0100

    .

---
+++ index.js
@@ -0,0 +1,109 @@
+'use strict'
+
+module.exports = attachComments
+
+var push = [].push
+
+function attachComments(tree, comments) {
+  walk(tree, {comments: comments.concat().sort(compare), index: 0})
+  return tree
+}
+
+function walk(node, state) {
+  var children = []
+  var comments = []
+  var key
+  var value
+  var index
+
+  // Done, we can quit.
+  if (state.index === state.comments.length) {
+    return
+  }
+
+  // Find all children of `node`
+  for (key in node) {
+    value = node[key]
+
+    // Ignore comments.
+    if (value && typeof value === 'object' && key !== 'comments') {
+      if (typeof value.type === 'string') {
+        children.push(value)
+      } else if (Array.isArray(value)) {
+        index = -1
+
+        while (++index < value.length) {
+          if (value[index] && typeof value[index].type === 'string') {
+            children.push(value[index])
+          }
+        }
+      }
+    }
+  }
+
+  // Sort the children.
+  children.sort(compare)
+
+  // Initial comments.
+  push.apply(
+    comments,
+    slice(state, node, false, {leading: true, trailing: false})
+  )
+
+  index = -1
+
+  while (++index < children.length) {
+    walk(children[index], state)
+  }
+
+  // Dangling or trailing comments.
+  push.apply(
+    comments,
+    slice(state, node, true, {
+      leading: false,
+      trailing: Boolean(children.length)
+    })
+  )
+
+  if (comments.length) {
+    node.comments = comments
+  }
+}
+
+function slice(state, node, compareEnd, fields) {
+  var result = []
+
+  while (
+    state.comments[state.index] &&
+    compare(state.comments[state.index], node, compareEnd) < 1
+  ) {
+    result.push(Object.assign({}, state.comments[state.index++], fields))
+  }
+
+  return result
+}
+
+function compare(left, right, compareEnd) {
+  var field = compareEnd ? 'end' : 'start'
+
+  // Offsets.
+  if (left.range && right.range) {
+    return left.range[0] - right.range[compareEnd ? 1 : 0]
+  }
+
+  // Points.
+  if (left.loc && left.loc.start && right.loc && right.loc[field]) {
+    return (
+      left.loc.start.line - right.loc[field].line ||
+      left.loc.start.column - right.loc[field].column
+    )
+  }
+
+  // Just `start` (and `end`) on nodes.
+  // Default in most parsers.
+  if ('start' in left && field in right) {
+    return left.start - right[field]
+  }
+
+  return NaN
+}
---
+++ test.js
@@ -0,0 +1,151 @@
+'use strict'
+
+var test = require('tape')
+var acorn = require('acorn')
+var recast = require('recast')
+var walk = require('estree-walker').walk
+var attachComments = require('.')
+
+test('estree-attach-comments (recast)', function (t) {
+  t.equal(
+    recast.print(attachComments(...parse(''))).code,
+    '',
+    'should support an empty document'
+  )
+
+  t.equal(
+    recast.print(attachComments(...parse('a + 1'))).code,
+    'a + 1;',
+    'should support no comments'
+  )
+
+  t.equal(
+    recast.print(attachComments(...parse('/* ! */'))).code,
+    '/* ! */\n',
+    'should support a single block comment'
+  )
+
+  t.equal(
+    recast.print(attachComments(...parse('// !'))).code,
+    '// !\n',
+    'should support a single line comment'
+  )
+
+  t.equal(
+    recast.print(
+      attachComments(...parse('/* 1 */ function a (/* 2 */b) { return b + 1 }'))
+    ).code,
+    '/* 1 */\nfunction a(\n    /* 2 */\n    b\n) {\n    return b + 1;\n}',
+    'should support some comments'
+  )
+
+  t.equal(
+    recast.print(
+      attachComments(
+        ...parse(
+          '/* 1 */ function /* 2 */ a /* 3 */ (/* 4 */b) /* 5 */ { /* 6 */ return /* 7 */ b + /* 8 */ 1 /* 9 */ }'
+        )
+      )
+    ).code,
+    '/* 1 */\nfunction /* 2 */\na(\n    /* 3 */\n    /* 4 */\n    b\n) /* 5 */\n{\n    /* 6 */\n    return (\n        /* 7 */\n        b + /* 8 */\n        1\n    );\n}/* 9 */',
+    'should support a bunch of block comments'
+  )
+
+  // Recast parses `4` as “dangling”:
+  // <https://github.com/benjamn/recast/blob/dd7c5ec/lib/comments.ts#L255-L256>
+  // But apprently doesn’t serialize it?
+  t.equal(
+    recast.print(
+      attachComments(...parse('/* 1 */ a /* 2 */ = /* 3 */ { /* 4 */ }'))
+    ).code,
+    '/* 1 */\na = /* 2 */\n/* 3 */\n{};',
+    'should support some more comments'
+  )
+
+  t.equal(
+    recast.print(
+      attachComments(
+        ...parse(
+          '// 1\nfunction // 2\na // 3\n(// 4\nb) // 5\n { // 6\n return b + // 7\n 1 // 8\n }'
+        )
+      )
+    ).code,
+    '// 1\nfunction // 2\na(\n    // 3\n    // 4\n    b\n) // 5\n{\n    // 6\n    return b + // 7\n    1;\n}// 8',
+    'should support a bunch of line comments'
+  )
+
+  var comments = []
+  var tree = acorn.parse('/* 1 */ a /* 2 */ + /* 3 */ 1', {
+    ecmaVersion: 2020,
+    onComment: comments
+  })
+
+  removePositions(tree)
+
+  t.equal(
+    recast.print(attachComments(tree, comments)).code,
+    'a + 1;',
+    'should not fail on a tree w/o positional info'
+  )
+
+  comments = []
+  tree = acorn.parse('/* 1 */ a /* 2 */ + /* 3 */ 1', {
+    ecmaVersion: 2020,
+    onComment: comments
+  })
+
+  removePositions(comments)
+
+  t.equal(
+    recast.print(attachComments(tree, comments)).code,
+    'a + 1;',
+    'should not fail on comments w/o positional info'
+  )
+
+  comments = []
+  tree = acorn.parse('/* 1 */ a /* 2 */ + /* 3 */ 1', {
+    ecmaVersion: 2020,
+    ranges: true,
+    onComment: comments
+  })
+
+  removePositions(tree)
+
+  t.equal(
+    recast.print(attachComments(tree, comments)).code,
+    '/* 1 */\na + /* 2 */\n/* 3 */\n1;',
+    'should use `range`s'
+  )
+
+  comments = []
+  tree = acorn.parse('/* 1 */ a /* 2 */ + /* 3 */ 1', {
+    ecmaVersion: 2020,
+    locations: true,
+    onComment: comments
+  })
+
+  removePositions(tree)
+
+  t.equal(
+    recast.print(attachComments(tree, comments)).code,
+    '/* 1 */\na + /* 2 */\n/* 3 */\n1;',
+    'should use `loc`s'
+  )
+
+  t.end()
+})
+
+function parse(doc) {
+  var comments = []
+  var tree = acorn.parse(doc, {ecmaVersion: 2020, onComment: comments})
+  return [tree, comments]
+}
+
+function removePositions(value) {
+  walk(value, {
+    enter: function (node) {
+      delete node.start
+      delete node.end
+    }
+  })
+}
